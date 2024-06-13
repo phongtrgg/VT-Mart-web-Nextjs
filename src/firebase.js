be5124108fde6af0ec4d9c3923b10bb480ed6b45
khanh <khanhlnfx20736@funix.edu.vn>
2024-06-13 09:44:21 +0700

    1st push

---
+++ src/firebase.js
@@ -0,0 +1,64 @@
+import { initializeApp, getApps, getApp } from 'firebase/app'
+import {
+    getMessaging,
+    getToken,
+    onMessage,
+    isSupported,
+} from 'firebase/messaging'
+import { useStoreFcm } from './hooks/react-query/push-notification/usePushNotification'
+
+const firebaseConfig = {
+    measurementId: '',
+    apiKey: 'AIzaSyAbWvwUvcSXjiN4XqFbQnjC84MIxXc_Bhw',
+    authDomain: 'test-dmhieu.firebaseapp.com',
+    projectId: 'test-dmhieu',
+    storageBucket: 'test-dmhieu.appspot.com',
+    messagingSenderId: '232386985329',
+    appId: '1:232386985329:web:e70bedb9ef281de1dbcad0',
+}
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const messaging = (async () => {
+    try {
+        const isSupportedBrowser = await isSupported()
+        if (isSupportedBrowser) {
+            return getMessaging(firebaseApp)
+        }
+
+        return null
+    } catch (err) {
+        return null
+    }
+})()
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+    return getToken(await messaging, {
+        vapidKey: '',
+    })
+        .then((currentToken) => {
+            if (currentToken) {
+                setTokenFound(true)
+                setFcmToken(currentToken)
+
+                // Track the token -> client mapping, by sending to backend server
+                // show on the UI that permission is secured
+            } else {
+                setTokenFound(false)
+                setFcmToken()
+                // shows on the UI that permission is required
+            }
+        })
+        .catch((err) => {
+            console.error(err)
+            // catch error while creating client token
+        })
+}
+
+export const onMessageListener = async () =>
+    new Promise((resolve) =>
+        (async () => {
+            const messagingResolve = await messaging
+            onMessage(messagingResolve, (payload) => {
+                resolve(payload)
+            })
+        })()
+    )
